9898d2c5bd6c15cb253bd0793c294629ef1382a4
Brian-Momata <brianmomata@gmail.com>
2022-08-08 09:07:10 +0300

    Add operate function that calls the math functions on the numbers depending on the operator

--- script.js
+++ script.js
@@ -9,4 +9,19 @@ function multiply(a,b){
 };
 function divide(a,b){
     return a / b;
-}
+};
+
+function operate(a, char, b) {
+    if (char == "x"){
+        return multiply(a,b);
+    }
+    if (char == "+"){
+        return add(a,b);
+    }
+    if (char == "-"){
+        return subtract(a,b);
+    }
+    if (char == "/"){
+        return divide(a,b);
+    }
+};

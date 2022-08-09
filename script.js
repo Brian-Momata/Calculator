273ab1470aedb6eee70e5ff68797de1150abaaae
Brian-Momata <brianmomata@gmail.com>
2022-08-09 14:39:10 +0300

    Fix divide character and Add storeInput function
    The funciton saves user inputs that the operate function requires.
    Equals sign has an event listener that runs the operate function and displays on the screen.

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function divide(a,b){
 };
 
 function operate(a, char, b) {
-    if (char == "x"){
+    if (char == "*"){
         return multiply(a,b);
     }
     if (char == "+"){
@@ -35,4 +35,36 @@ operands.forEach((button) => {
     button.addEventListener("click", (e) => {
     
     screen.textContent += e.target.value;
+    storeInput();
 })});
+
+const operators = document.querySelectorAll(".operators");
+operators.forEach((button) => {
+    screen.textContent = "";
+    button.addEventListener("click", (e) => {
+    
+    screen.textContent += e.target.value;
+    storeInput();
+})});
+
+
+let num1;
+let num2;
+let character;
+let numbers;
+
+function storeInput(){
+    numbers = screen.textContent;
+    for (let i = 0; i < numbers.length; i++){
+        if (numbers[i] == "+" || numbers[i] == "-" || numbers[i] == "/" || numbers[i] == "*"){
+            character = numbers[i];
+            num1 = parseInt(numbers.slice(0, i));
+            num2 = parseInt(numbers.slice( i + 1, numbers.length));
+        }
+    }
+};
+
+const equals = document.querySelector(".operate");
+equals.addEventListener("click", function(){
+    screen.textContent = operate(num1, character, num2);
+})
